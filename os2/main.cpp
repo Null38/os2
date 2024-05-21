87ba4312849120641261587a69ceef100510551a
null 38 <leeho2866@gmail.com>
2024-05-22 00:47:42 +0900

    구현 준비

--- os2/main.cpp
+++ os2/main.cpp
@@ -8,45 +8,76 @@
 
 using namespace std;
 
-#define THRESHOLD 10
-
+void enqueue();//이거 void인거 나중에 바꿀 수 있다는점 인지하기.
+void dequeue();
+void promote();
+void split_n_merge();
 char** parse(const char*);
+
+enum processType
+{
+	Foreground, Background
+};
 
 typedef struct ProcessNode
 {
-	//value? or void (*proc)()?
+	//쓰레드를 가지고 있어야할듯 아마 thread proc;
+	struct StakNode* parent;
 	ProcessNode* next;
 } procNode;
 
 typedef struct StakNode
 {
 	int procSize = 0;
-	procNode* procList;
-	//StakNode *prev?
-	StakNode* next;
+	procNode* procList = nullptr;
+	StakNode* prev = nullptr;
+	StakNode* next = nullptr;
 } stakNode;
 
-void enqueue()
-{
-
-}
-
-void dequeue()
-{
-
-}
+stakNode* stakBottom = new stakNode;
+stakNode* stakTop = stakBottom;
 
 int main(int argc, char* argv[])
 {
-	//stakNode* head = new stakNode;//staktop이 필요한데...
 	//stack top이란?..
-	//shell과 monitor프로세스 추가해야함.
+	//shell(fore)과 monitor(back)프로세스 추가해야함.
 
 	char** p = parse(" test ads ;  &12  34 ");
 
 	return 0;
 }
 
+void enqueue()
+{
+	procNode* proc = NULL/* 수정해야 할 부분. 프로세스가 foregroun인가? */ == Foreground ? stakTop->procList : stakBottom->procList;
+
+	for (; proc->next != nullptr; proc = proc->next);
+
+	//Add(proc, /*process*/); // 클래스로 선언해서 proc.Add(/*process*/)로 변경해보기는? 연결리스트가 2개라 그게 더 깔끔할수도?
+}
+
+void dequeue()
+{
+	//어떤 스택의 프로세스를 디스패치하는것?.. 인자를 받아야하는가?
+	//if(procIsEnd) proc.RemoveProc(); -> in RemoveProc() if (procList == nullptr) Remove()
+}
+
+void promote()
+{
+	//enqueue() 또는dequeue() 수행시 실행됨
+	//내가 stakTop일경우 다음 스택노드는? 상위는 nullptr이다. 과제를 보면 top인경우엔 다시 bottom으로 가는건가?
+	//꼬리에 붙여라. 인자를 받을 수 있다면 구현은 일단 쉬울듯.
+}
+
+void split_n_merge()
+{
+	//enqueue() 또는 promote() 수행시 실행됨
+	//threshold = 전체프로세스개수/ 스택노드수.. stak과 proc리스트를 class로 한후 static변수로 해두는게 맞을듯. 그렇게 관리하는게 나을 듯 함.
+	//if(my->procList.Count() <= threshold) return; 실행할 필요 없네라는 판단.
+	//절반을 자른다.. RemoveProc()에 몇개를 자를지 설정할 수 있는 오버로딩을 추가하는게 좋을듯. 구현한다면...
+	//만약 my == StakTop? stak.Add();
+	//이걸 일단 수행하고 재귀적으로 다시 호출. 재귀가 끝날때는 위같은 경우겠지.
+}
 
 char** parse(const char* command)
 {
