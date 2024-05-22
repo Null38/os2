6b6a8906fa10edaf9a05edc1c012123358405a0b
null 38 <leeho2866@gmail.com>
2024-05-23 03:04:29 +0900

    클래스로 변환 및 주석으로 각각 todo 추가

--- os2/main.cpp
+++ os2/main.cpp
@@ -14,28 +14,104 @@ void promote();
 void split_n_merge();
 char** parse(const char*);
 
+class StakNode;
+class ProcList;
+
+
+#pragma region StakNode
+class StakNode
+{
+public:
+	StakNode();
+	//procNode를 받는 오버로딩 구현하기
+	~StakNode();
+	int Count();
+
+	ProcList* procList;//readonly가 없는가?..수정하지 못하게 _procList로 하고 procList함수로구현해볼까
+	//캡슐화를 위해 밑에 변수들을 프라이빗으로 바꾸고 get만들어야한다.
+	StakNode* prev = nullptr;
+	StakNode* next = nullptr;
+	//필요한 함수는? Add Remove
+private:
+	int procSize;
+	static int nodeSize;
+};
+
+int StakNode::nodeSize = 0;
+
+int StakNode::Count()
+{
+	return nodeSize;
+}
+
+StakNode::~StakNode()
+{
+	nodeSize--;
+}
+
+#pragma endregion StakNode
+
+#pragma region ProcNode
+
+class ProcList
+{
+public:
+	struct ProcNode;
+	ProcList(StakNode*);
+	ProcList(StakNode*, ProcNode*);
+	~ProcList();
+	//캡슐화 하기
+	//필요한 함수는? Add Remove split. split은 일정 크기 자르고 주소값 리턴하자. 이를 위해 ProcNode를 받는 생성자를 추가해야할듯
+	
+
+private:
+	//쓰레드를 가지고 있어야할듯 아마 thread proc;
+	StakNode* parent;
+	ProcNode* start;
+	ProcNode* end;
+};
+
+struct ProcList::ProcNode
+{
+	ProcNode* next = nullptr;
+};
+
+ProcList::ProcList(StakNode* parent)
+{
+	this->parent = parent;
+	this->start = nullptr;
+	this->end = nullptr;
+}
+
+ProcList::ProcList(StakNode* parent, ProcNode* newNode)
+{
+	this->parent = parent;
+	start = newNode;
+	for (end = start; end->next != nullptr; end = end->next);
+}
+
+ProcList::~ProcList()
+{
+}
+	
+
+#pragma endregion ProcNode
+
+StakNode::StakNode()//선언 문제로 여기다가..
+{
+	procList = new ProcList(this); 
+	this->procSize = 0;
+	nodeSize++;
+}
+
+
 enum processType
 {
 	Foreground, Background
 };
 
-typedef struct ProcessNode
-{
-	//쓰레드를 가지고 있어야할듯 아마 thread proc;
-	struct StakNode* parent;
-	ProcessNode* next;
-} procNode;
-
-typedef struct StakNode
-{
-	int procSize = 0;
-	procNode* procList = nullptr;
-	StakNode* prev = nullptr;
-	StakNode* next = nullptr;
-} stakNode;
-
-stakNode* stakBottom = new stakNode;
-stakNode* stakTop = stakBottom;
+StakNode* stakBottom = new StakNode();
+StakNode* stakTop = stakBottom;
 
 int main(int argc, char* argv[])
 {
@@ -49,9 +125,8 @@ int main(int argc, char* argv[])
 
 void enqueue()
 {
-	procNode* proc = NULL/* 수정해야 할 부분. 프로세스가 foregroun인가? */ == Foreground ? stakTop->procList : stakBottom->procList;
+	//if 프로세스 == foregroun? stakTop에 추가 : Bottom에 추가.
 
-	for (; proc->next != nullptr; proc = proc->next);
 
 	//Add(proc, /*process*/); // 클래스로 선언해서 proc.Add(/*process*/)로 변경해보기는? 연결리스트가 2개라 그게 더 깔끔할수도?
 }
@@ -65,14 +140,15 @@ void dequeue()
 void promote()
 {
 	//enqueue() 또는dequeue() 수행시 실행됨
-	//내가 stakTop일경우 다음 스택노드는? 상위는 nullptr이다. 과제를 보면 top인경우엔 다시 bottom으로 가는건가?
-	//꼬리에 붙여라. 인자를 받을 수 있다면 구현은 일단 쉬울듯.
+	//P가 따로 존재.
+	//P가 stakTop일경우 다음 스택노드는 bottom으로.
+	//꼬리에 붙여라. 인자넣어도 되겠지 뭐
 }
 
 void split_n_merge()
 {
 	//enqueue() 또는 promote() 수행시 실행됨
-	//threshold = 전체프로세스개수/ 스택노드수.. stak과 proc리스트를 class로 한후 static변수로 해두는게 맞을듯. 그렇게 관리하는게 나을 듯 함.
+	//threshold = 전체프로세스개수/ 스택노드수. stak.ProcCount() / StakNode.Count()
 	//if(my->procList.Count() <= threshold) return; 실행할 필요 없네라는 판단.
 	//절반을 자른다.. RemoveProc()에 몇개를 자를지 설정할 수 있는 오버로딩을 추가하는게 좋을듯. 구현한다면...
 	//만약 my == StakTop? stak.Add();
